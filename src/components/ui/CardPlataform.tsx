467281589ddd6886eb6bd66a0770fc7f62dbb72d
Gildo-Henriques <gildohenriques520@gmail.com>
2025-03-27 22:04:12 +0100

    versão 0.0.0.1 Beta G

--- src/components/ui/CardPlataform.tsx
+++ src/components/ui/CardPlataform.tsx
@@ -1,10 +1,10 @@
-import StartupProfile from "@/app/(ui)/feed/startup/[id]/page";
+
 import BomSvg from "../svgs/BomSvg";
 import BusinessLogoCrescimentoFinanceiro from "../svgs/CrescimentoSvg";
 import LogoLampada from "../svgs/LampadaSvg";
-import StartupSvg from "../svgs/StartupSvg";
+
 import StartupsSvg from "../svgs/StartupsSvg";
-import LogoCOSMOS from "./LogoCOSMOS";
+
 
 export default function CardDesign(){
     return (
@@ -18,52 +18,52 @@ export default function CardDesign(){
         //     <div className="w-[90%] lg:h-20 h-16 top-44 rounded-lg bg-gradient-to-t from-[#0f0f0f] via-[#0a0a0a] to-[#080808]  absolute border-full-card"></div>
         //     <div className="w-[90%] h-16 top-72 rounded-lg bg-white/30  absolute border-full-card"></div>
         // </div>
-        <div className="lg:w-[450px] w-full  h-[480px] relative">
-            <div className="h-full w-full absolute top-0 left-0 bg-gradient-to-b from-transparent to-black  "></div>
-            <div className="border-full space-y-5 rounded-4xl px-5 w-full h-full">
-            <div className="w-full space-x-2 flex justify-center py-5 mx-auto">
-                <div className="w-14 rounded-xl border-full"></div>
-                <div className="size-2 rounded-full border-full"></div>
+        <div className="z-20 lg:w-[450px] w-full  h-[480px] relative">
+            <div className="z-20 h-full w-full absolute top-0 left-0 bg-gradient-to-b from-transparent to-black  "></div>
+            <div className=" z-20 border-full backdrop-blur-lg space-y-5 rounded-4xl px-5 w-full h-full">
+            <div className="z-20 w-full space-x-2 flex justify-center py-5 mx-auto">
+                <div className="z-20 w-14 rounded-xl border-full"></div>
+                <div className="z-20 size-2 rounded-full border-full"></div>
             </div>
-            <div className="lg:w-[400px] w-full items-center px-5 h-20 bg-white/10 backdrop-blur-md rounded-lg flex justify-between lg:-ml-20">
-           <div className="flex space-x-3">
-           <div className="bg-white/5 rounded-lg p-3 backdrop-blur-lg">
+            <div className="z-20 lg:w-[400px] w-full items-center px-5 h-20 bg-white/10 backdrop-blur-md rounded-lg flex justify-between lg:-ml-20">
+           <div className="z-20 flex space-x-3">
+           <div className="z-20 bg-white/5 rounded-lg p-3 backdrop-blur-lg">
            <StartupsSvg/>
            </div>
            <div className="">
             <h2>+500 Startups</h2>
-            <p className="text-xs text-white/50">Startups no mercado Angolano</p>
+            <p className="z-20 text-xs text-white/50">Startups no mercado Angolano</p>
            </div>
            </div>
-           <div className="text-green-500">
+           <div className="z-20 text-green-500">
             <BomSvg />
            </div>
             </div>
-            <div className="w-full h-16 bg-gradient-to-b from-white/10 to-white/5 flex justify-between px-3 items-center rounded-lg">
-            <div className="flex space-x-3">
-                <div className="bg-white/5 rounded-lg p-3">
+            <div className="z-20 w-full h-16 bg-gradient-to-b from-white/10 to-white/5 flex justify-between px-3 items-center rounded-lg">
+            <div className="z-20 flex space-x-3">
+                <div className="z-20 bg-white/5 rounded-lg p-3">
                 <BusinessLogoCrescimentoFinanceiro size="size-6"/>
                 </div>
                 <div className="">
             <h2>+100 Investidores</h2>
-            <p className="text-xs text-white/50">Dispostos a investir em startups</p>
+            <p className="z-20 text-xs text-white/50">Dispostos a investir em startups</p>
            </div>
             </div>
-            <div className="text-green-500">
+            <div className="z-20 text-green-500">
             <BomSvg />
            </div>
             </div>
-            <div className="w-full h-16 bg-gradient-to-b from-white/10 to-white/5 flex justify-between px-3 items-center rounded-lg">
-            <div className="flex space-x-3">
-                <div className="bg-white/5 rounded-lg p-3">
+            <div className="z-20 w-full h-16 bg-gradient-to-b from-white/10 to-white/5 flex justify-between px-3 items-center rounded-lg">
+            <div className="z-20 flex space-x-3">
+                <div className="z-20 bg-white/5 rounded-lg p-3">
                 <LogoLampada />
                 </div>
                 <div className="">
             <h2>+50 Talentos</h2>
-            <p className="text-xs text-white/50">Profissionais dispostos a participar em startups</p>
+            <p className="z-20 text-xs text-white/50">Profissionais dispostos a participar em startups</p>
            </div>
             </div>
-            <div className="text-green-500">
+            <div className="z-20 text-green-500">
             <BomSvg />
            </div>
             </div>
